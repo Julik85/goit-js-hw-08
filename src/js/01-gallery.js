ec8bf78ecf16937b53241c626b2252a2f80bee5e
Julik85 <mironuk@ukr.net>
2023-03-13 19:19:19 +0200

    ch1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const createGallery = createGaleryMarkup (galleryItems);
+
+galleryEl.insertAdjacentHTML('afterbegin', createGallery)
+
+function createGaleryMarkup (galleryItems) {
+return galleryItems.map(({ preview, original, description }) => {
+    return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img 
+                class="gallery__image"
+                src="${preview}" 
+                data-source="${original}"
+                alt="${description}" /> 
+            </a>           
+        </li>
+        `;}).join('');
+   
+};
+
+new SimpleLightbox('.gallery a', {   
+    captionDelay: 250,    
+    captionsData: 'alt',    
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
+const formEl = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('.feedback-form input');
+const textareaEl = document.querySelector('.feedback-form textarea');
 
+const LOCAL_NAME = 'feedback-form-state';
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+populateForm();
+
+function onFormInput() {
+  const formData = {
+    emailEl: emailEl.value,
+    textareaEl: textareaEl.value,
+  };
+  localStorage.setItem(LOCAL_NAME, JSON.stringify(formData));
+//   console.log(formData);
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (emailEl.value === '' || textareaEl.value === '') {
+    return alert ('Заповніть всі поля!');
+    }
+
+  const saveLocalStorage = localStorage.getItem(LOCAL_NAME);
+  const objectFromLocalStorage = JSON.parse(saveLocalStorage);
+  console.log(objectFromLocalStorage);
+
+  localStorage.removeItem(LOCAL_NAME);
+  event.currentTarget.reset();
+}
+
+function populateForm() {
+  const saveLocalStorage = localStorage.getItem(LOCAL_NAME);
+  const objectFromLocalStorage = JSON.parse(saveLocalStorage);
+  if (objectFromLocalStorage) {
+    emailEl.value = objectFromLocalStorage.emailEl;
+    textareaEl.value = objectFromLocalStorage.textareaEl;
+  }
+}
