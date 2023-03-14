06bf899f1191d95b1baa51f0d592ea6718b0199e
Julik85 <mironuk@ukr.net>
2023-03-14 12:31:52 +0200

    ch2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,20 +3,28 @@ import Player from '@vimeo/player';
 
     const iframe = document.querySelector('iframe');
     const player = new Player(iframe);
+  const timeKey = 'videoplayer-current-time';
    const timeUpdate = function (time) {
-  localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
+  localStorage.setItem(timeKey, time.seconds);
 };
 
 player.on('timeupdate', throttle(timeUpdate, 1000));
 
-const saveTime = localStorage.getItem('videoplayer-current-time');
-const timeStop = JSON.parse(saveTime);
+const saveTime = localStorage.getItem(timeKey);
+const timeStop = saveTime;
 
-try {
-  player.setCurrentTime(timeStop.seconds || 0);
-} catch (error) {
-  console.log(error.message);
+currentTime ();
+function currentTime () {
+  if (timeStop) {
+      player.setCurrentTime(timeStop)
+  };
 }
+
+// try {
+//   player.setCurrentTime(timeStop.seconds || 0);
+// } catch (error) {
+//   console.log(error.message);
+// }
    
 
     
